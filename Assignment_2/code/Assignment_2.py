c6946c9d2e043472ccc45f7c3e052ff7ddc46f52
Matish007 <70635841+Matish007@users.noreply.github.com>
2020-09-06 13:53:48 +0530

    Update Assignment_2.py

--- Assignment_2/code/Assignment_2.py
+++ Assignment_2/code/Assignment_2.py
@@ -3,14 +3,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math as m
 from mpl_toolkits.mplot3d import Axes3D
-fig = plt.figure()
-ax = fig.add_subplot(211, projection='3d')# subplot for Plane
+fig
+ax = fig.add_subplot(311, projection='3d')# subplot for Plane
 normal=np.array([3,5,-6])
-d=7/m.sqrt(70)
-xx,yy=np.meshgrid(range(50),range(50))
-z = (-normal[0]*xx -normal[1]*yy -d)*1./normal[2]
+d=7*m.sqrt(70)
+# a plane is a*x+b*y+c*z+d=0
+# [a,b,c] is the normal. Thus, we have to calculate
+# d and we're set
+d = -7*m.sqrt(70)
+
+# create x,y
+xx, yy = np.meshgrid(range(30), range(30))
+
+# calculate corresponding z
+z = (-normal[0] * xx - normal[1] * yy - d) * 1. /normal[2]
+
+# plot the surface
 plt3d = plt.figure().gca(projection='3d')
-plt3d.plot_surface(xx,yy,z, color='blue')
+plt3d.plot_surface(xx, yy, z)
+ax = fig.add_subplot(312, projection='3d')# subplot for Plane
+normal=np.array([3,5,-6])
+# a plane is a*x+b*y+c*z+d=0
+# [a,b,c] is the normal. Thus, we have to calculate
+# d and we're set
+d = 7*m.sqrt(70)
+
+# create x,y
+xx, yy = np.meshgrid(range(30), range(30))
+
+# calculate corresponding z
+z = (-normal[0] * xx - normal[1] * yy - d) * 1. /normal[2]
+
+# plot the surface
+plt3d = plt.figure().gca(projection='3d')
+plt3d.plot_surface(xx, yy, z)
+
+fig=plt.figure()
+
 ax = fig.add_subplot(212, projection='3d')#subplot for normal vector
 
 x_pos = 0
@@ -21,6 +50,8 @@ z_pos=0
 z1_direct=-6
 
 ax.quiver(x_pos,y_pos,x_direct,y_direct,z_pos,z1_direct)
+
+
 
 
 plt.show()
