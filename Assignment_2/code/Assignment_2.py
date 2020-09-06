aabf747a4f19b9e939c0e5ecb5612a72530f4adf
Matish007 <70635841+Matish007@users.noreply.github.com>
2020-09-06 16:21:51 +0530

    Update Assignment_2.py

--- Assignment_2/code/Assignment_2.py
+++ Assignment_2/code/Assignment_2.py
@@ -1,58 +1,46 @@
-
-import matplotlib.pyplot as plt
 import numpy as np
-import math as m
+import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-fig
-ax = fig.add_subplot(311, projection='3d')# subplot for Plane
-normal=np.array([3,5,-6])
-d=7*m.sqrt(70)
+%matplotlib inline
+import seaborn as sns
+import math as m
+
+# set normal vector, and point on plane
+point=np.array([0,0,0])
+normal = np.array([3, 5, -6])
+
 # a plane is a*x+b*y+c*z+d=0
 # [a,b,c] is the normal. Thus, we have to calculate
 # d and we're set
 d = -7*m.sqrt(70)
 
 # create x,y
-xx, yy = np.meshgrid(range(30), range(30))
+xx, yy = np.meshgrid(range(10), range(10))
 
 # calculate corresponding z
-z = (-normal[0] * xx - normal[1] * yy - d) * 1. /normal[2]
+z = (-normal[0] * xx - normal[1] * yy -d) * 1. /normal[2]
+
+# setup plot
+plt3d = plt.figure(figsize=(10,10)).gca(projection='3d');
+plt3d.set_xlabel('x');
+plt3d.set_ylabel('y');
+plt3d.set_zlabel('z');
 
 # plot the surface
-plt3d = plt.figure().gca(projection='3d')
-plt3d.plot_surface(xx, yy, z)
-ax = fig.add_subplot(312, projection='3d')# subplot for Plane
-normal=np.array([3,5,-6])
-# a plane is a*x+b*y+c*z+d=0
-# [a,b,c] is the normal. Thus, we have to calculate
-# d and we're set
-d = 7*m.sqrt(70)
+plt3d.plot_surface(xx, yy, z, alpha=0.7);
 
-# create x,y
-xx, yy = np.meshgrid(range(30), range(30))
+# plot the point
+plt3d.plot([point[0]], [point[1]], [point[2]], color='yellow', marker='o', markersize=10, alpha=0.8);
 
-# calculate corresponding z
-z = (-normal[0] * xx - normal[1] * yy - d) * 1. /normal[2]
+# set the normal vector to start at the center of the plane
+startX = np.mean(plt3d.get_xlim())
+startY = np.mean(plt3d.get_ylim())
+startZ = (-normal[0] * startX - normal[1] * startY - d) * 1. /normal[2]
 
-# plot the surface
-plt3d = plt.figure().gca(projection='3d')
-plt3d.plot_surface(xx, yy, z)
+# set the normal vector to start at the point on the plane
+startX = point[0]
+startY = point[1]
+startZ = point[2]
 
-fig=plt.figure()
-
-ax = fig.add_subplot(212, projection='3d')#subplot for normal vector
-
-x_pos = 0
-y_pos = 0
-x_direct = 3
-y_direct = 5
-z_pos=0
-z1_direct=-6
-
-ax.quiver(x_pos,y_pos,x_direct,y_direct,z_pos,z1_direct)
-
-
-
-
-plt.show()
-Axes3D.plot()
+# plot the normal vector
+plt3d.quiver([startX], [startY], [startZ], [normal[0]], [normal[1]], [normal[2]], linewidths = (5,), edgecolor="red");
